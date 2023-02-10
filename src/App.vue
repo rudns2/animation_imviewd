8f38301a107bd9131b2e296c9103ca12cb344837
gyeo <104889999+rudns2@users.noreply.github.com>
2023-02-10 17:46:21 +0900

    내가 본 애니 목록

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
+  <manga />
+  <div></div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import manga from './components/manga.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    manga
   }
 }
 </script>
@@ -22,5 +26,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+*{
+  padding : 0;
+  margin : 0;
+  list-style: none;
 }
 </style>
---
+++ src/components/manga.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+      <div class="ani_img_wrap">
+        <ul>
+          <li v-for="item in animation" :key="item">
+            <h1>{{ item.aniName }}</h1>
+            <img :src="require(`@/assets/img/${item.aniImg}`)" alt="">
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        animation: [{
+            aniName: '어둠의 실력자가 되고싶어서!',
+            aniImg: 'ani1.png'
+          },
+          {
+            aniName: '터무니없는 스킬로 이세계방랑밥',
+            aniImg: 'ani2.jpg'
+          },
+          {
+            aniName: '해골기사님은 지금 이세계 모험중',
+            aniImg: 'ani3.jpg'
+          },
+          {
+            aniName: '블루록',
+            aniImg: 'ani4.jpg'
+          },
+          {
+            aniName: '현실주의 용사의 왕국 재건기',
+            aniImg: 'ani5.jpg'
+          },
+          {
+            aniName: '봇치 더 락',
+            aniImg: 'ani6.jpg'
+          },
+        ]
+      }
+    }
+  }
+</script>
+<style scoped>
+  .ani_img_wrap {
+    padding: 0 2rem;
+  }
+
+  .ani_img_wrap ul {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 10px;
+
+  }
+
+  .ani_img_wrap ul li {
+    width: 24%;
+    margin-top: 15px;
+  }
+
+  .ani_img_wrap ul li h1 {
+    font-size: 18px;
+  }
+
+  .ani_img_wrap ul li img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
